379193d34c43577ad794a5446698afe3d0ecd88c
Nii ga <yzxjason@live.cn>
2018-05-18 15:49:41 +0800

    Update 6.1.c

--- mooc Cdown/6/6.1.c
+++ mooc Cdown/6/6.1.c
@@ -1,6 +1,9 @@
 /*
-  获得长字符
-  获得短字符
+输入样例2：
+This is my book[回车]
+at[回车]
+输出样例2：
+No[回车]
 */
 #include <stdio.h>
 #include <malloc.h>
@@ -10,10 +13,9 @@ int main()
     char *s, *t;
     s = (char *)malloc(1024); 
     t = (char *)malloc(1024); 
-    gets(s);    // 输入一个包含空格的字符串输入，长字符
-    gets(t);    // 输入短字符
-
-
+    gets(s);    // 获得一个包含空格的字符串输入，长字符
+    gets(t);    // 获得短字符
+    
     free(s);
     free(t);
     return 0;
