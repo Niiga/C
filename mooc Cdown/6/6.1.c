ec7f7f26a457aa9e28c3c202ae695e99a3af983e
Nii ga <yzxjason@live.cn>
2018-05-18 15:50:30 +0800

    Update 6.1.c

--- mooc Cdown/6/6.1.c
+++ mooc Cdown/6/6.1.c
@@ -1,4 +1,9 @@
 /*
+输入样例1：
+This is his book[回车]
+is[回车]
+输出样例1：
+3[回车]
 输入样例2：
 This is my book[回车]
 at[回车]
@@ -15,7 +20,7 @@ int main()
     t = (char *)malloc(1024); 
     gets(s);    // 获得一个包含空格的字符串输入，长字符
     gets(t);    // 获得短字符
-    
+
     free(s);
     free(t);
     return 0;
