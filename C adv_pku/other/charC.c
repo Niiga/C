08ba947ec7ecf946e57398ab9a3550b92f7f1083
Nii ga <yzxjason@live.cn>
2018-05-30 14:48:54 +0800

    up

---
+++ C adv_pku/other/charC.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+typedef struct Inv {
+	char data;
+	double num;  
+    struct Inv *next; 
+}
+
+int main()
+{
+	struct Inv *pr, *head;
+	head = (struct Inv*)malloc(sizeof(struct Inv));
+	pr = head;
+
+	int a[3], b[3], i, j;
+	char ch;
+	float n, sA[3] = {0.0}, s1[3] = {0.0};
+
+	for (i=0; i<3; i++) {
+		scanf_s("%d %d", &a[i], &b[i]);
+		for (j=0; j<b[i]; j++) {
+			pr->next = (struct Inv*)malloc(sizeof(struct Inv));
+			p = p->next;
+			
+			ch = getchar();
+			scanf_s("%f", &n);
+			pr.data = ch;
+			pr.num = n;
+			s1[i] += n;
+			if ( ch == 'A') {
+				s[0] += n;
+			} else if (ch == 'B') {
+				s[1] += n;
+			} else if (ch == 'C') {
+				s[2] += n;
+			}
+		}
+	}
+	
+
+
+	return 0;
+} 
---
+++ C adv_pku/week 2/ti.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+typedef struct {
+    char ch[1;
+    float a;
+}node[1000];
+
+int main()
+{
+    
+}
