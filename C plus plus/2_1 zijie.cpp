51717ee49c01673ea9d1275d6814aab01a6c5c4e
Nii ga <yzxjason@live.cn>
2018-08-23 14:16:17 +0800

    Create 2_1 zijie.cpp

---
+++ C plus plus/2_1 zijie.cpp
@@ -0,0 +1,16 @@
+//#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "The size of an int is:\t\t" << sizeof(int) << " bytes.\n";
+    cout << "The size of an long is:\t\t" << sizeof(long) << " bytes.\n";
+    cout << "The size of an long long is:\t" << sizeof(long long) << " bytes.\n";
+    cout << "The size of an char is:\t\t" << sizeof(char) << " bytes.\n";
+    cout << "The size of an double is:\t" << sizeof(double) << " bytes.\n";
+//	cout << "The size of an int is :" << " bytes.\n";
+	
+    return 0;
+}
+
